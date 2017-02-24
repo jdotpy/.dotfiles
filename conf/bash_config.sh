7cf924b469b181086799fbd3f8ce268f4fa0d798
= <=>
2017-02-24 12:42:11 -0500

    Added vim docs, git completion, and prompt config

--- conf/bash_config.sh
+++ conf/bash_config.sh
@@ -1,4 +1,25 @@
 PATH="$PATH:~/.dotfiles/bin"
 
+source ~/.dotfiles/conf/git-completion.bash
+
 alias untarball="tar -xzf"
 alias tarball="tar -czf"
+
+prompt() {
+  git_head_output="$(git symbolic-ref HEAD 2>&1)"
+  
+  if [[ $git_head_output == *"ref HEAD is not a symbolic ref"* ]]; then
+    branch_name="!Detached Head!"
+  elif [[ $git_head_output == *"Not a git repository"* ]]; then
+    branch_name=""
+  else 
+    branch_name=${git_head_output##refs/heads/}
+  fi
+  if [ -n "$branch_name" ]; then 
+    export PS1="\e[34m[\t] me \e[39m\w\e[93m {$branch_name}\e[34m$\e[39m "
+  else
+    export PS1="\e[34m[\t] me \e[39m\w\e[93m\e[34m$\e[39m "
+  fi
+}
+
+PROMPT_COMMAND=prompt
