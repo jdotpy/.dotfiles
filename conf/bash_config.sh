e4380cc5d13788942a182877f9a8672c5a7f7733
KJ <Redacted>
2017-02-27 21:29:36 -0500

    Added virtualenv support to my bash prompt

--- conf/bash_config.sh
+++ conf/bash_config.sh
@@ -6,20 +6,33 @@ alias untarball="tar -xzf"
 alias tarball="tar -czf"
 
 prompt() {
+  ################
+  # Git Repo
   git_head_output="$(git symbolic-ref HEAD 2>&1)"
-  
   if [[ $git_head_output == *"ref HEAD is not a symbolic ref"* ]]; then
-    branch_name="!Detached Head!"
+    ps1_git_status="!Detached Head!"
   elif [[ $git_head_output == *"Not a git repository"* ]]; then
-    branch_name=""
+    ps1_git_status=""
   else 
-    branch_name=${git_head_output##refs/heads/}
+    ps1_git_status=${git_head_output##refs/heads/}
   fi
-  if [ -n "$branch_name" ]; then 
-    export PS1="\[\e[34m\][\t] me \[\e[39m\]\w\[\e[93m\] {$branch_name}\[\e[34m\]$\[\e[39m\] "
-  else
-    export PS1="\[\e[34m\][\t] me \[\e[39m\]\w\[\e[34m\]$\[\e[39m\] "
+  if [ -n "$ps1_git_status" ]; then 
+    ps1_git_status="{$ps1_git_status}"
   fi
+
+  ################
+  # Virtual Env
+  if [ -n "$VIRTUAL_ENV" ]; then
+    ps1_virtual_env=$(basename "$VIRTUAL_ENV")
+    ps1_virtual_env="($ps1_virtual_env)"
+  else 
+    unset ps1_virtual_env;
+  fi
+  
+
+  ################
+  # PS1
+  export PS1="\[\e[34m\][\t] me:\[\e[96m\]$ps1_virtual_env\[\e[93m\]$ps1_git_status \[\e[39m\]\w \[\e[34m\]$\[\e[39m\] "
 }
 
 PROMPT_COMMAND=prompt
