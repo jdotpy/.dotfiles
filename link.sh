80ffa7169ac43adcd28aadc14291baafa71d14fd
KJ <jdotpy@users.noreply.github.com>
2019-04-26 09:57:01 -0400

    Fixed link process

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 ln -sf ~/.dotfiles/conf/vimrc ~/.vimrc 
 ln -sf ~/.dotfiles/conf/vim ~/.vim
 
-printf "source ~/.dotfiles/conf/bash_config.sh" >> ~/.bashrc
+printf "\nsource ~/.dotfiles/conf/bash_config.sh\n" >> ~/.bashrc
 
 # Finish by forcing a refresh
 source ~/.bashrc
