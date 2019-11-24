dc00ff8d48a97552cbda63304b7850677121f86a
kj <jdotpy@users.noreply.github.com>
2019-11-23 22:46:19 -0500

    Upgrading to Ctrl-P

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@ ln -sf ~/.dotfiles/conf/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/conf/vim ~/.vim
 
 printf "\nsource ~/.dotfiles/conf/bash_config.sh\n" >> ~/.bashrc
+printf "\nsource ~/.dotfiles/conf/bash_config.sh\n" >> ~/.zshrc
 
 # Finish by forcing a refresh
 source ~/.bashrc
